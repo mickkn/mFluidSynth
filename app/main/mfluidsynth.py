f29a283e0b32db2cb2832aa8562bd2f18d3afeb4
Mick Kirkegaard <mickkn@gmail.com>
2021-04-28 23:24:30 +0200

    Work on windows now, also.

--- app/main/mfluidsynth.py
+++ app/main/mfluidsynth.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import platform
+import sys
 
 FLUIDSYNTH_DEFAULT_ARGS = {
 
@@ -15,22 +16,22 @@ FLUIDSYNTH_DEFAULT_ARGS = {
     "-g": "1",  # Set the master gain [0 < gain < 10, default = 0.2]
     # "-G": "",   # Defines the number of LADSPA audio nodes
     # "-h": "",   # Print out this help summary
-    #"-i": "",   # Don't read commands from the shell [default = yes]
+    # "-i": "",   # Don't read commands from the shell [default = yes]
     # "-j": "",   # Attempt to connect the jack outputs to the physical ports
     # "-K": "",   # The number of midi channels [default = 16]
     # "-L": "",   # The number of stereo audio channels [default = 1]
     # "-m": "alsa_seq",   # The name of the midi driver to use. Valid values: winmidi
     # "-n": "",   # Don't create a midi driver to read MIDI input events [default = yes]
-    #"-o": "shell.port=9800",  # Define a setting, -o name=value ("-o help" to dump current values)
+    # "-o": "shell.port=9800",  # Define a setting, -o name=value ("-o help" to dump current values)
     # "-O": "",   # Audio file format for fast rendering or aufile driver ("help" for list)
     "-p": "fluid",  # Set MIDI port name (alsa_seq, coremidi drivers)
-    #"-q": "",   # Do not print welcome message or other informational output
+    # "-q": "",   # Do not print welcome message or other informational output
     # (Windows only: also suppress all log messages lower than PANIC
     "-r": "48000",  # Set the sample rate
     "-R": "0",  # Turn the reverb on or off [0|1|yes|no, default = on]
-    #"-s": "",  # Start FluidSynth as a server process
+    # "-s": "",  # Start FluidSynth as a server process
     # "-T": "",   # Audio file type for fast rendering or aufile driver ("help" for list)
-    #"-v": "",   # Print out verbose messages about midi events (synth.verbose=1) as well as other debug messages
+    # "-v": "",   # Print out verbose messages about midi events (synth.verbose=1) as well as other debug messages
     # "-V": "",   # Show version of program
     # "-z": "",   # Size of each audio buffer
 
@@ -46,7 +47,8 @@ fluidsynth = None
 
 class Fluidsynth:
 
-    def __init__(self, path: str = "fluidsynth", midi: str = "Keystation 49 MK3", soundfont: str = "", arguments: dict = None) -> None:
+    def __init__(self, path: str = "fluidsynth", midi: str = "Keystation 49 MK3", soundfont: str = "",
+                 arguments: dict = None) -> None:
 
         global fluidsynth
 
@@ -60,12 +62,12 @@ class Fluidsynth:
         self.instruments = None
 
         for name, value in list(self.arguments_dict.items()):
-            #self.arguments = self.arguments + f" {name}{value}"
+            # self.arguments = self.arguments + f" {name}{value}"
             self.arguments.append(f"{name}{value}")
 
         if platform.system() == 'Linux':
-                self.arguments.append("-aalsa")
-                self.arguments.append("-malsa_seq")
+            self.arguments.append("-aalsa")
+            self.arguments.append("-malsa_seq")
 
         self.arguments.append(soundfont)
 
@@ -77,13 +79,13 @@ class Fluidsynth:
             elif platform.system() == 'Linux':
                 print("OS: Linux")
                 fluidsynth.kill()
-                #if midi_device is not None:
+                # if midi_device is not None:
                 #    midi_device.kill()
             else:
                 print("Unknown OS")
                 sys.exit()
 
-        print( "debug:    Starting subprocess")
+        print("debug:    Starting subprocess")
         print(f"debug:    Soundfont: {soundfont}")
         print(f"debug:    Args: {self.arguments}")
 
@@ -111,7 +113,7 @@ class Fluidsynth:
 
             # Be nice and report the noob, that his keyboard is not connected or the name is wrong
             connection = subprocess.Popen(["aconnect", "-i"], stdout=subprocess.PIPE)
-            
+
             stdout_lines = ""
             for line in connection.stdout:
                 stdout_lines = stdout_lines + line.decode('ascii').strip()
@@ -158,7 +160,7 @@ class Fluidsynth:
         breaker = 0
         fluidsynth.stdout.flush()
         for line in fluidsynth.stdout:
-            #print(line)
+            # print(line)
             if "> " in line:
                 breaker += 1
             if breaker >= 2:
